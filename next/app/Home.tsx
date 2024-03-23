a575fe4e2bf2d752fc27672a87ddd2b56d9812ae
Annie He <anniehe1126@gmail.com>
2024-03-23 01:54:12 -0400

    signup

--- next/app/Home.tsx
+++ next/app/Home.tsx
@@ -3,53 +3,143 @@
 import styles from "./page.module.scss";
 import { useState } from 'react';
 import { useSession, signIn, signOut } from "next-auth/react";
-// import { getServerSession } from "next-auth/next"
+import bcrypt from 'bcryptjs';
+import { PrismaClient } from '@prisma/client';
 
-async function handleSignIn() {
-  const result = await signIn("credentials", {
-    redirect: false, // Prevents redirection after sign-in
-    email: "annie.he@cooper.edu",
-    password: "anniepassword",
-  })
+async function handleSignOut() {
+  const result = await signOut({ redirect: false });
 }
 
 export default function Home() {
   const { data: session } = useSession();
+  const [uname, setUname] = useState('');
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [dob, setDOB] = useState('');
+  const prisma = new PrismaClient()
 
-  // const result = await signIn("credentials", {
-  //   redirect: false, // Prevents redirection after sign-in
-  //   email: "annie.he@cooper.edu",
-  //   password: "anniepassword",
-  // })
+  const handleSignIn = async (e: any) => {
+    e.preventDefault();
+    const result = await signIn('credentials', { 
+      redirect: false,
+      email, 
+      password 
+    });
 
-  // if (result?.error) {
-  //   // Handle error here, e.g., show an error message
-  //   console.error(result.error)
-  // } else {
-  //   console.log(result);
-  // }
+    if (result?.error) {
+      console.error(result.error);
+    } else {
+      console.log(result);
+    }
+  };
+  const handleSignUp = async (e: any) => {
+    e.preventDefault();
+    console.log( JSON.stringify({
+      email, uname, password, fname, lname, dob
+    }));
+    const response = await fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email, uname, password, fname, lname, dob
+      }),
+    });
+  
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error('Signup failed:', errorData.error);
+    } else {
+      console.log("success");
+    }
+  };
 
-  // const session = await getServerSession(authOptions);
+  if (session?.user) {
+    return (
+      <div className={`${styles.flexc} ${styles.full}`}>
+        <div>You are {`${session.user.name}`}</div>
+        <button onClick={handleSignOut}>Log out</button>
+      </div>
+    )
+  } else {
+    return (
+      <div className={`${styles.flexr} ${styles.full}`}>
+        <form onSubmit={handleSignIn} className={`${styles.flexc} ${styles.full}`}>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          <button type="submit">Sign In</button>
+        </form>
+        <form onSubmit={handleSignUp} className={`${styles.flexc} ${styles.full}`}>
+            <label htmlFor="uname">Username:</label>
+            <input
+              type="uname"
+              id="uname"
+              value={uname}
+              onChange={(e) => setUname(e.target.value)}
+              required
+            />
+            <label htmlFor="fname">Firstname:</label>
+            <input
+              type="fname"
+              id="fname"
+              value={fname}
+              onChange={(e) => setFname(e.target.value)}
+              required
+            />
+            <label htmlFor="lname">Lastname:</label>
+            <input
+              type="lname"
+              id="lname"
+              value={lname}
+              onChange={(e) => setLname(e.target.value)}
+              required
+            />
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label htmlFor="dob">Date of Birth:</label>
+            <input
+              type="date"
+              id="dob"
+              value={dob}
+              onChange={(e) => setDOB(e.target.value)}
+              required
+            />
 
-  // console.log("session:", session);
-
-  // if (session) {
-  //   // Use the session data
-  //   return <div>Welcome</div>;
-  // } else {
-  //   return <div>You are not logged in why?</div>;
-  // }
-
-  function handleCheck() {
-    console.log(session);
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <button onClick={handleSignIn}>Sign in</button>
-      <button onClick={handleCheck}>Check</button>
-    </div>
-  );
-
 }
 
---
+++ next/app/api/signup/route.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import { NextRequest } from "next/server";
+
+const prisma = new PrismaClient();
+
+async function signUp(req: NextRequest) {
+
+  // Extract the data from the request body
+  const body = await req.json();
+  const { email, uname, password, fname, lname, dob } = body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  let res;
+  try {  
+    await prisma.user.create({
+      data: {
+        email: email,
+        uname: uname,
+        password: hashedPassword,
+        first_name: fname,
+        last_name: lname,
+        dob: new Date(dob),
+      },
+    });
+    res = new Response(JSON.stringify("user created"), { status: 201 });
+  } catch (err) {
+    res = new Response(JSON.stringify("database error"), { status: 418 });
+  } finally {
+    await prisma.$disconnect();
+    return res;
+  }
+}
+
+export { signUp as GET, signUp as POST };
