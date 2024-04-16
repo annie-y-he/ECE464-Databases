3be773d366988d236ae5c2ce69b98dc9d088dad2
Annie He <anniehe1126@gmail.com>
2024-04-16 18:23:42 -0400

    sudo

--- docker.sh
+++ docker.sh
@@ -5,14 +5,14 @@ PROJECT_NAME="sqlib"
 
 if [ "$1" = "rm" ]; then
   echo "Stopping and removing containers..."
-  docker-compose -p $PROJECT_NAME down -v
+  sudo docker compose -p $PROJECT_NAME down -v
 elif [ $1 = "db" ]; then
-  docker exec -it ${PROJECT_NAME}-db-1 env PGPASSWORD='inkcherry' psql -U annie -d lib
+  sudo docker exec -it ${PROJECT_NAME}-db-1 env PGPASSWORD='inkcherry' psql -U annie -d lib
 elif [ $1 = "app" ]; then
-  docker exec -it ${PROJECT_NAME}-app-1 bash
+  sudo docker exec -it ${PROJECT_NAME}-app-1 bash
 else
   echo "Starting services..."
-  docker-compose -p $PROJECT_NAME up -d
+  sudo docker compose -p $PROJECT_NAME up -d
   echo "Containers are running in the background."
 fi
 echo "Exec into mysql with:"
