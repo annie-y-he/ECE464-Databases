1e66306af12ed528614ad6b368967adeded3e0b9
Annie He <anniehe1126@gmail.com>
2024-04-17 01:16:10 -0400

    bp not needed in link

--- next/app/Nav.tsx
+++ next/app/Nav.tsx
@@ -2,7 +2,6 @@
 
 import styles from "./page.module.scss";
 import Link from 'next/link'
-const BP = process.env.NEXT_PUBLIC_BASE_PATH;
 const buttons = ["library", "shelf", "profile", "search"]
 
 export default function Nav({
@@ -10,15 +9,12 @@ export default function Nav({
 }: {
   children: React.ReactNode;
 }) {
-  buttons.forEach((btn) => {
-    console.log(BP + "/" + btn);
-  })
   return (
     <div className={styles.main}>
       <div className={styles.viewer}>{children}</div>
       <div className={styles.nav}>
         {buttons.map((btn, i) => (
-          <Link key={i} className={styles.navbtn} href={BP + "/" + btn}>{btn}</Link>
+          <Link key={i} className={styles.navbtn} href={btn}>{btn}</Link>
         ))}
       </div>
     </div>
