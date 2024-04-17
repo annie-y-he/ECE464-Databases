393e99e8e50a0565ab4dc0f1c18258566a80524a
Annie He <anniehe1126@gmail.com>
2024-04-17 02:03:30 -0400

    session provider bp

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -4,11 +4,11 @@ import Nav from "./Nav";
 import Home from "./Home";
 import { getSession } from "next-auth/react";
 import { SessionProvider } from "next-auth/react"
-
+const BP = process.env.NEXT_PUBLIC_BASE_PATH;
 
 export default function (children: any, pageProps: any) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session} basePath={BP + 'api/auth'}>
       <Nav>
         <Home />
       </Nav>
