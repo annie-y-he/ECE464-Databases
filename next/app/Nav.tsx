52eabbe98eb385c92f3c072df4c01e98a2a224f2
Annie He <anniehe1126@gmail.com>
2024-04-17 00:39:11 -0400

    nextauth full url

--- next/app/Nav.tsx
+++ next/app/Nav.tsx
@@ -15,7 +15,7 @@ export default function Nav({
       <div className={styles.viewer}>{children}</div>
       <div className={styles.nav}>
         {buttons.map((btn, i) => (
-          <Link key={i} className={styles.navbtn} href={btn}>{btn}</Link>
+          <Link key={i} className={styles.navbtn} href={"/databases/" + btn}>{btn}</Link>
         ))}
       </div>
     </div>
