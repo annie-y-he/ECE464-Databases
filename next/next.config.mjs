67cab6eb8be618102633980a4e881fb7e6d88049
Annie He <anniehe1126@gmail.com>
2024-04-16 20:32:15 -0400

    assetprefix

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -12,14 +12,15 @@ const nextConfig = {
     // Return the modified config
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: '/_next/:path*',
-        destination: '/databases/_next/:path*'
-      }
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/_next/:path*',
+  //       destination: '/databases/_next/:path*'
+  //     }
+  //   ];
+  // },
+  assetPrefix: '/databases',
 };
 
 export default nextConfig;
