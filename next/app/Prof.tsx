0ca3845ec9322d6ed2072ecc1093519b88ed0304
Syyrion <cuiricky4@gmail.com>
2024-05-06 19:36:55 -0400

    remove example test inputs

--- next/app/Prof.tsx
+++ next/app/Prof.tsx
@@ -26,32 +26,21 @@ export default function Prof() {
 
   return (
     <div className={s.prof}>
-      <div>value</div>
-      <input type="text" onChange={(e) => {setField1(e.target.value);
-        console.log(e.target.value)
-      }} value={field1}/>
-      <div>type</div>
-      <input type="text" onChange={(e) => {setField2(e.target.value);
-        console.log(e.target.value)
-      }} value={field2}/>  
-      <button onClick={
-        (e) => handler({value: field1, type: field2})
-      }>submit</button>    
-      <div>old password</div>
+      <div>Old Password</div>
       <input type="text" />
-      <div>new password</div>
+      <div>New Password</div>
       <input type="text" />
-      <div>dob</div>
+      <div>DOB</div>
       <input type="text" />
-      <div>email</div>
+      <div>Email</div>
       <input type="text" />
-      <div>uname</div>
+      <div>Username</div>
       <input type="text" />
       <div>image</div>
       <input type="file" />
-      <div>first_name</div>
+      <div>First Name</div>
       <input type="text" />
-      <div>last_name</div>
+      <div>Last Name</div>
       <input type="text" />
       <br />
       <button>save</button><button>delete</button><button>cancel</button>
